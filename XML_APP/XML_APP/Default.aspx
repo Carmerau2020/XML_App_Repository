a229f26f2d8a3139082c56144914e624c40259e7
Carmerau2020 <jaworska.katarzyna81@gmail.com>
2020-06-16 12:06:23 +0100

    Add new files

---
+++ XML_APP/XML_APP/Default.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="XML_APP._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+   <center>
+        <h2>Inport/Export Database data to XML file</h2>
+   </center>
+    <div>
+        <br />
+        <table>
+            <tr>
+                <td>
+                    Select file:
+                </td>
+                <td>
+                    <asp:FileUpload ID="FileUpload1" runat="server" />
+                </td>
+                <td>
+                    <asp:Button ID="btnImport" runat ="server" Text="Import" OnClick="btnImport_Click" />    
+                </td>
+            </tr>
+        </table>
+        <div>
+            <br />
+            <center>
+                <asp:Label ID="lbMessage" runat="server" Font-Bold="true" />
+            </center>
+            <br />
+            <asp:GridView ID="gvData" runat="server" AutoGenerateColumns="false">
+                <EmptyDataTemplate>
+                    <div style="padding:10px">
+                        Data not found.
+                    </div>
+                </EmptyDataTemplate>
+                <Columns>
+                    <asp:BoundField HeaderText="Employee ID" DataField="EmployeeID" ItemStyle-Width="100"/>
+                    <asp:BoundField HeaderText="Company Name" DataField="CompanyName" ItemStyle-Width="260" />
+                    <asp:BoundField HeaderText="Contact Name" DataField="ContactName" ItemStyle-Width="160"/>
+                    <asp:BoundField HeaderText="Contact Title" DataField="ContactTitle" ItemStyle-Width="160"/>
+                    <asp:BoundField HeaderText="Address" DataField="EmployeeAddress" ItemStyle-Width="360"/>
+                    <asp:BoundField HeaderText="Postal Code" DataField="PostalCode" ItemStyle-Width="160"/>
+                </Columns>
+            </asp:GridView>
+            <br />
+            <asp:Button ID="btnExport" runat="server" Text="Export" OnClick="btnExport_Click" />
+        </div>
+    </div>
+       
+</asp:Content>
---
+++ XML_APP/XML_APP/Default.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+
+namespace XML_APP
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                PopulateData();
+                lbMessage.Text = "Current Database Data";
+            }
+        }
+        private void PopulateData()
+        {
+            using(XMLDatabaseEntities1 db = new XMLDatabaseEntities1())
+            {
+                gvData.DataSource = db.EmployeeMaster.ToList();
+                gvData.DataBind();
+            }
+
+        }
+
+        protected void btnImport_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.PostedFile.ContentType == "aplication/xml" || FileUpload1.PostedFile.ContentType == "text/xml")
+            {
+                string fileName;
+                IList<EmployeeMaster> emList;
+                try
+                {
+                    fileName = Path.Combine(Server.MapPath("~/UploadDocuments"), Guid.NewGuid().ToString() + ".xml");
+                    FileUpload1.PostedFile.SaveAs(fileName);
+                }
+                catch(Exception)
+                {
+                    throw;
+                }
+                try {
+                    XDocument xDoc = XDocument.Load(fileName);
+                    emList = xDoc.Descendants("Employee").Select(d =>
+                        new EmployeeMaster
+                        {
+                            EmployeeID = d.Element("EmployeeID").Value,
+                            CompanyName = d.Element("CompanyName").Value,
+                            ContactName = d.Element("ContactName").Value,
+                            ContactTitle = d.Element("ContactTitle").Value,
+                            EmployeeAddress = d.Element("EmployeeAddress").Value,
+                            PostalCode = d.Element("PostalCode").Value
+                        }).ToList();
+                    //Update Data
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                try
+                {
+
+                    using (XMLDatabaseEntities1 db = new XMLDatabaseEntities1())
+                    {
+                        foreach (var i in emList)
+                        {
+                            var v = db.EmployeeMaster.Where(a => a.EmployeeID.Equals(i.EmployeeID)).FirstOrDefault();
+                            if (v != null)
+                            {
+                                v.CompanyName = i.CompanyName;
+                                v.ContactName = i.ContactName;
+                                v.ContactTitle = i.ContactTitle;
+                                v.EmployeeAddress = i.EmployeeAddress;
+                                v.PostalCode = i.PostalCode;
+                            }
+                            else
+                            {
+                                db.EmployeeMaster.Add(i);
+                            }
+                        }
+                        db.SaveChanges();
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                try
+                {
+                    PopulateData();
+                    lbMessage.Text = "Import done Successfully";
+
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+        protected void btnExport_Click(object sender, EventArgs e)
+        {
+            using(XMLDatabaseEntities1 db = new XMLDatabaseEntities1())
+            {
+                List<EmployeeMaster> emList = db.EmployeeMaster.ToList();
+                if (emList.Count > 0)
+                {
+                    var xEle = new XElement("Employees",
+                        from emp in emList
+                        select new XElement("Employee",
+                            new XElement("EmployeeID", emp.EmployeeID),
+                            new XElement("CompanyName", emp.CompanyName),
+                            new XElement("ContactName", emp.ContactName),
+                            new XElement("ContactTitle", emp.ContactTitle),
+                            new XElement("EmployeeAddress", emp.EmployeeAddress),
+                            new XElement("PostalCode", emp.PostalCode)
+                            ));
+                    HttpContext context = HttpContext.Current;
+                    context.Response.Write(xEle);
+                    context.Response.ContentType = "aplication/xml";
+                    context.Response.AppendHeader("Content-disposition", "atachment; filename=EmployeeData.xml");
+                    context.Response.End();
+                }
+            }
+        }
+    }
+}
